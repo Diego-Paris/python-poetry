83d9864aeaaba72fcd1a6811ed10f94b8ee53252
diego-paris <diego.paris@upr.edu>
2022-05-06 04:33:13 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Python Poetry Dependency Management Test Project
+
+[Poetry docs](https://python-poetry.org/docs/basic-usage/)
---
+++ poetry_test/main.py
@@ -0,0 +1,11 @@
+import pendulum
+
+
+def main():
+    print('WOWEE!')
+    w = pendulum.date(1, 1, 1)
+    print(w)
+
+
+if __name__ == '__main__':
+    main()
